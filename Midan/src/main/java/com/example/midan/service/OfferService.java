2cd3d7d8390d44eed15c1cb8cde6b0abe714552e
nikolanushev <nikola_nusev@yahoo.com>
2022-02-17 16:54:30 +0100

    Updates

--- Midan/src/main/java/com/example/midan/service/OfferService.java
+++ Midan/src/main/java/com/example/midan/service/OfferService.java
@@ -1,4 +1,14 @@
 package com.example.midan.service;
 
+import com.example.midan.model.Enumerations.OfferType;
+import com.example.midan.model.Offer;
+
+import java.util.List;
+
 public interface OfferService {
+    List<Offer> findAll();
+    Offer findById(Long id);
+    Offer create(String offerFor, String offerName, OfferType type);
+    Offer update(Long id, String offerFor, String offerName, OfferType type);
+    Offer delete(Long id);
 }
