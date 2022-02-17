4837ff17ce96c786ba222c43f46ac8316de74816
nikolanushev <nikola_nusev@yahoo.com>
2022-02-17 16:46:03 +0100

    Updates

--- Midan/src/main/java/com/example/midan/service/RoomService.java
+++ Midan/src/main/java/com/example/midan/service/RoomService.java
@@ -1,4 +1,14 @@
 package com.example.midan.service;
 
+import com.example.midan.model.Enumerations.RoomType;
+import com.example.midan.model.Room;
+
+import java.util.List;
+
 public interface RoomService {
+    List<Room> listAll();
+    Room findById(Long id);
+    Room create(String name, Double price, RoomType type);
+    Room update(Long id, String name, Double price, RoomType type);
+    Room delete(Long id);
 }
