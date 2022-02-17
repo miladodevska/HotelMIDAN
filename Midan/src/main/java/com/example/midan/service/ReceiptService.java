8e7365116ae245bd7fc0a3d0efc2402326e9ebd8
nikolanushev <nikola_nusev@yahoo.com>
2022-02-17 16:50:19 +0100

    Updates

--- Midan/src/main/java/com/example/midan/service/ReceiptService.java
+++ Midan/src/main/java/com/example/midan/service/ReceiptService.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface ReceiptService {
     List<Receipt> listAllReceipts();
     Receipt findById(Long id);
-    Receipt create();
-    Receipt update();
+    Receipt create(Integer roomNumber, Double price);
+    Receipt update(Long id, Integer roomNumber, Double price);
     Receipt delete(Long id);
 }
